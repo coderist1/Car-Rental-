ee1f30332ce6e1665b5241d7de75cce265fe31a4
Coneybelle Baquiro <baquiro.coneybelle@gmail.com>
2026-03-05 15:42:48 +0800

    Complete routing setup in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { VehicleProvider } from './context/VehicleContext';
+import { LogReportProvider } from './context/LogReportContext';
 import { useAuth } from './hooks';
 import {
   Login,
@@ -50,80 +51,82 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <VehicleProvider>
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-            <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-            <Route path="/admin-register" element={<PublicRoute><AdminRegister /></PublicRoute>} />
+          <LogReportProvider>
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+              <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+              <Route path="/admin-register" element={<PublicRoute><AdminRegister /></PublicRoute>} />
 
-            {/* Owner Routes */}
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute allowedRoles={['owner']}>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
+              {/* Owner Routes */}
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute allowedRoles={['owner']}>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* Protected Renter Routes */}
-            <Route
-              path="/renter"
-              element={
-                <ProtectedRoute allowedRoles={['renter']}>
-                  <RenterDashboard />
-                </ProtectedRoute>
-              }
-            />
+              {/* Protected Renter Routes */}
+              <Route
+                path="/renter"
+                element={
+                  <ProtectedRoute allowedRoles={['renter']}>
+                    <RenterDashboard />
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* Admin Routes */}
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute allowedRoles={['admin']}>
-                  <AdminDashboard />
-                </ProtectedRoute>
-              }
-            />
+              {/* Admin Routes */}
+              <Route
+                path="/admin"
+                element={
+                  <ProtectedRoute allowedRoles={['admin']}>
+                    <AdminDashboard />
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* Shared Protected Routes */}
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/change-password"
-              element={
-                <ProtectedRoute>
-                  <ChangePassword />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/bookings"
-              element={
-                <ProtectedRoute>
-                  <Bookings />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/email-log"
-              element={
-                <ProtectedRoute allowedRoles={['admin']}>
-                  <EmailLog />
-                </ProtectedRoute>
-              }
-            />
+              {/* Shared Protected Routes */}
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/change-password"
+                element={
+                  <ProtectedRoute>
+                    <ChangePassword />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/bookings"
+                element={
+                  <ProtectedRoute>
+                    <Bookings />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/email-log"
+                element={
+                  <ProtectedRoute allowedRoles={['admin']}>
+                    <EmailLog />
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* Default Redirect */}
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          </Routes>
+              {/* Default Redirect */}
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
+            </Routes>
+          </LogReportProvider>
         </VehicleProvider>
       </AuthProvider>
     </BrowserRouter>
