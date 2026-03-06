19b6fd42ac76636a7ebb67dcb48777e5ddd12a24
Mathew Polinar <Mathewpolinar5@gmail.com>
2026-03-06 12:36:24 +0800

    Updates

--- src/components/navigation/ProfileMenu.jsx
+++ src/components/navigation/ProfileMenu.jsx
@@ -33,7 +33,6 @@ const ROLE_COLORS = {
 
 
 
-// SVG icons
 
 const PersonIcon = () => (
 
@@ -72,9 +71,7 @@ const BookingsIcon = () => (
 
 
 const EmailIcon = () => (
-
   <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" aria-hidden="true">
-
     <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
 
   </svg>
@@ -242,7 +239,7 @@ function ProfileMenu() {
 
 
 
-          {/* ΓöÇΓöÇ Header: avatar + name + role ΓöÇΓöÇ */}
+          {     }
 
           <div className="pm-header">
 
@@ -280,7 +277,7 @@ function ProfileMenu() {
 
 
 
-          {/* ΓöÇΓöÇ View Profile button ΓöÇΓöÇ */}
+          {}
 
           {!isAdmin && (
 
@@ -314,7 +311,7 @@ function ProfileMenu() {
 
 
 
-          {/* ΓöÇΓöÇ Menu items ΓöÇΓöÇ */}
+          {}
 
           <div className="pm-items" role="none">
 
@@ -398,7 +395,7 @@ function ProfileMenu() {
 
 
 
-          {/* ΓöÇΓöÇ Logout ΓöÇΓöÇ */}
+          { }
 
           <button 
 
